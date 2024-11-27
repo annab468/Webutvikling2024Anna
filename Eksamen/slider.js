bcc6ddf338f82a20219789a9d35e73e7539b333e
annab468 <annabanasiak468@gmail.com>
2024-11-27 19:08:07 +0100

    slider and banner working!

--- Eksamen/slider.js
+++ Eksamen/slider.js
@@ -4,3 +4,18 @@ const btnLeft = document.querySelector('.slider-btn.left');
 const btnRight = document.querySelector('.slider-btn.right');
 
 let currentSlide = 0;
+
+
+btnLeft.addEventListener('click', () => {
+    currentSlide = (currentSlide > 0) ? currentSlide - 1 : sliderItems.length - 1;
+    updateSlider();
+});
+
+btnRight.addEventListener('click', () => {
+    currentSlide = (currentSlide < sliderItems.length - 1) ? currentSlide + 1 : 0;
+    updateSlider();
+});
+
+function updateSlider() {
+    sliderContainer.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
