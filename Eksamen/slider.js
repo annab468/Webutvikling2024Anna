9eefa974922e0ce75d57b188c970d490930c23d0
annab468 <annabanasiak468@gmail.com>
2024-11-27 13:16:10 +0100

    js for slider

---
+++ Eksamen/slider.js
@@ -0,0 +1,6 @@
+const sliderContainer = document.querySelector('.slider-container');
+const sliderItems = document.querySelectorAll('.slider-item');
+const btnLeft = document.querySelector('.slider-btn.left');
+const btnRight = document.querySelector('.slider-btn.right');
+
+let currentSlide = 0;
